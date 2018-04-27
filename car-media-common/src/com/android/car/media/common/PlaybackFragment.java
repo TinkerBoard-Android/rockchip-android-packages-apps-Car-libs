92adabec95278d28284173567df9fafe2f26ccf7
Roberto Perez <robertoalexis@google.com>
2018-04-26 19:16:28 -0700

    Adding queue button and bringing playback controls styles to spec.
    Bug: 78604386
    Test: Launched media app on Mojave
    Change-Id: I273eb2cfa43ed51387c2268018e88df72fcb8434

--- car-media-common/src/com/android/car/media/common/PlaybackFragment.java
+++ car-media-common/src/com/android/car/media/common/PlaybackFragment.java
@@ -18,14 +18,6 @@ package com.android.car.media.common;
 
 import android.car.Car;
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.Canvas;
-import android.graphics.Paint;
-import android.graphics.PorterDuff;
-import android.graphics.PorterDuffXfermode;
-import android.graphics.Rect;
-import android.graphics.drawable.BitmapDrawable;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -137,55 +129,6 @@ public class PlaybackFragment extends Fragment {
             mAlbumBackground.setImageBitmap(null, true);
         }
         mAppName.setText(mediaSource.getName());
-        Bitmap cropped = getCircleCroppedBitmap(drawableToBitmap(mediaSource.getPackageIcon()));
-        mAppIcon.setImageBitmap(cropped);
-    }
-
-    /**
-     * Converts the given {@link Drawable} into a {@link Bitmap}
-     */
-    private Bitmap drawableToBitmap(Drawable drawable) {
-        Bitmap bitmap = null;
-
-        if (drawable instanceof BitmapDrawable) {
-            BitmapDrawable bitmapDrawable = (BitmapDrawable) drawable;
-            if (bitmapDrawable.getBitmap() != null) {
-                return bitmapDrawable.getBitmap();
-            }
-        }
-
-        if (drawable.getIntrinsicWidth() <= 0 || drawable.getIntrinsicHeight() <= 0) {
-            bitmap = Bitmap.createBitmap(1, 1, Bitmap.Config.ARGB_8888);
-        } else {
-            bitmap = Bitmap.createBitmap(drawable.getIntrinsicWidth(),
-                    drawable.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
-        }
-
-        Canvas canvas = new Canvas(bitmap);
-        drawable.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
-        drawable.draw(canvas);
-        return bitmap;
-    }
-
-    /**
-     * Crops the given bitmap into a circle with the same dimensions as the original one.
-     */
-    private Bitmap getCircleCroppedBitmap(Bitmap bitmap) {
-        Bitmap output = Bitmap.createBitmap(bitmap.getWidth(), bitmap.getHeight(),
-                Bitmap.Config.ARGB_8888);
-        Canvas canvas = new Canvas(output);
-
-        final int color = 0xff424242;
-        final Paint paint = new Paint();
-        final Rect rect = new Rect(0, 0, bitmap.getWidth(), bitmap.getHeight());
-
-        paint.setAntiAlias(true);
-        canvas.drawARGB(0, 0, 0, 0);
-        paint.setColor(color);
-        canvas.drawCircle(bitmap.getWidth() / 2, bitmap.getHeight() / 2,
-                bitmap.getWidth() / 2f, paint);
-        paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
-        canvas.drawBitmap(bitmap, rect, rect, paint);
-        return output;
+        mAppIcon.setImageBitmap(mediaSource.getRoundPackageIcon());
     }
 }
