9c750a97734f931ed6f745fd9c034cee0e5b2d69
Lujiang Xue <rogerxue@google.com>
2018-06-06 10:52:48 -0700

    migrate to androidx
    Fixes: 109812231
    
    Test: manually
    Change-Id: I461ecd67efa6be97da741aa1fdb8c383c17d2ef8

--- car-list/Android.mk
+++ car-list/Android.mk
@@ -23,7 +23,10 @@ LOCAL_SRC_FILES := $(call all-java-files-under, src)
 LOCAL_USE_AAPT2 := true
 
 LOCAL_STATIC_ANDROID_LIBRARIES := \
-    android-support-car
+    androidx.annotation_annotation \
+    androidx.car_car \
+    androidx.cardview_cardview \
+    androidx.recyclerview_recyclerview
 
 LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
 
--- car-list/src/com/android/car/list/SpinnerLineItem.java
+++ car-list/src/com/android/car/list/SpinnerLineItem.java
@@ -17,7 +17,6 @@
 package com.android.car.list;
 
 import android.content.Context;
-import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -25,6 +24,8 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.android.car.list.R;
 
