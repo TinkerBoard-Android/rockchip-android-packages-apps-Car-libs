8deec2a8c7eb5bf5fcf5ca801728e1f1049223f4
Ritwika Mitra <ritwikam@google.com>
2019-07-22 13:36:04 -0700

    DO NOT MERGE Create unit tests for Car Assist Client Lib
    Also, add testing to ensure isCarCompatibleMessagingNotification works.
    
    Bug: 137654440, 128924398
    Test: is a test
    Change-Id: I6a72bd80190a796b334800a9e000e4e488e2ac3e

--- car-assist-client-lib/tests/Android.mk
+++ car-assist-client-lib/tests/Android.mk
@@ -1,4 +1,3 @@
-#
 # Copyright (C) 2019 The Android Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,34 +11,9 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
 
-LOCAL_PATH:= $(call my-dir)
-
+LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
-LOCAL_SRC_FILES := $(call all-java-files-under, src)
-
-LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
-
-LOCAL_MODULE := car-assist-client-lib
-
-LOCAL_MODULE_TAGS := optional
-
-LOCAL_PRIVILEGED_MODULE := true
-
-LOCAL_PROGUARD_ENABLED := disabled
-
-LOCAL_USE_AAPT2 := true
-
-LOCAL_STATIC_ANDROID_LIBRARIES += \
-    androidx.legacy_legacy-support-v4 \
-
-LOCAL_STATIC_JAVA_LIBRARIES += \
-    car-assist-lib \
-
-include $(BUILD_STATIC_JAVA_LIBRARY)
-
-ifeq (,$(ONE_SHOT_MAKEFILE))
-    include $(call all-makefiles-under,$(LOCAL_PATH))
-endif
+# Include all makefiles in subdirectories
+include $(call all-makefiles-under,$(LOCAL_PATH))
