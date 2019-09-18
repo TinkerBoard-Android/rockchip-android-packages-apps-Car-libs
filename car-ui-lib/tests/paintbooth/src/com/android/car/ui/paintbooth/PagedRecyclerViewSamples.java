d231eaaad756358f0dd7b406a713b9d79566ce6a
Priyank Singh <priyanksingh@google.com>
2019-09-18 15:18:07 -0700

    PRV should adjust according to Toolbar height if registered.
    Test: Manual
    Change-Id: I5bbd546a302554b7da1a8cfcc034a56c851e77b1

---
+++ car-ui-lib/tests/paintbooth/src/com/android/car/ui/paintbooth/PagedRecyclerViewSamples.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright (C) 2019 The Android Open Source Project.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.car.ui.paintbooth;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.recyclerview.widget.LinearLayoutManager;
+
+import com.android.car.ui.pagedrecyclerview.PagedRecyclerView;
+import com.android.car.ui.toolbar.Toolbar;
+
+import java.util.ArrayList;
+
+/**
+ * Activity that shows PagedRecyclerView example with dummy data.
+ */
+public class PagedRecyclerViewSamples extends Activity {
+
+    private final ArrayList<String> mData = new ArrayList<>();
+    private final int mDataToGenerate = 15;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.paged_recycler_view_samples);
+        PagedRecyclerView recyclerView = findViewById(R.id.list);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        RecyclerViewAdapter adapter = new RecyclerViewAdapter(generateDummyData());
+        recyclerView.setAdapter(adapter);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setState(Toolbar.State.SUBPAGE);
+        toolbar.addListener(new Toolbar.Listener() {
+            @Override
+            public void onBack() {
+                finish();
+            }
+        });
+    }
+
+    private ArrayList<String> generateDummyData() {
+        for (int i = 0; i <= mDataToGenerate; i++) {
+            mData.add("data" + i);
+        }
+        return mData;
+    }
+}
+
