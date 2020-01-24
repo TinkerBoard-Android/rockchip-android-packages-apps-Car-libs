39020e49882c212084c5184d47468da3d66f0a58
Ram Parameswaran <rampara@google.com>
2020-01-23 17:50:54 -0800

    Remove CarUiRobolectricTestRunner
    Fixes: 148249036
    Test: Run all tests in Robotests
    
    Change-Id: Ib925d436c5fabc94c712fa12717b9969dbebc40b

--- car-ui-lib/tests/robotests/src/com/android/car/ui/recyclerview/CarUiSmoothScrollerTest.java
+++ car-ui-lib/tests/robotests/src/com/android/car/ui/recyclerview/CarUiSmoothScrollerTest.java
@@ -22,14 +22,13 @@ import static com.google.common.truth.Truth.assertThat;
 
 import android.content.Context;
 
-import com.android.car.ui.CarUiRobolectricTestRunner;
-
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
 import org.robolectric.RuntimeEnvironment;
 
-@RunWith(CarUiRobolectricTestRunner.class)
+@RunWith(RobolectricTestRunner.class)
 public class CarUiSmoothScrollerTest {
 
     private Context mContext;
