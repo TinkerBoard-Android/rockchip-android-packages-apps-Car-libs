b3a35147c14a753accfe3c2691128bdba29a760c
Cole Faust <colefaust@google.com>
2020-05-27 17:56:05 -0700

    DO NOT MERGE: Add an RTL RRO
    This RRO moves the back button and tabs to the left in RTL.
    We want the important navigation items to be closer to the driver,
    even if it breaks convential RTL layouts.
    
    Bug: 155418943
    Bug: 155418645
    Bug: 154241156
    Test: Manually
    
    Change-Id: I3f47739bc0b591e5a729bb62e96c5e2f73b41cf0

---
+++ car-ui-lib/referencedesign/product.mk
@@ -0,0 +1,35 @@
+# Inherit from this product to include the "Reference Design" RROs for CarUi
+
+# Include built-time overlays
+PRODUCT_PACKAGE_OVERLAYS += \
+    packages/apps/Car/libs/car-ui-lib/referencedesign/overlay/
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
+    packages/apps/Car/libs/car-ui-lib/referencedesign/overlay/
+
+# Include generated RROs
+PRODUCT_PACKAGES += \
+    googlecarui-com-android-car-ui-paintbooth \
+    googlecarui-com-android-car-rotaryplayground \
+    googlecarui-com-android-car-themeplayground \
+    googlecarui-com-android-car-carlauncher \
+    googlecarui-com-android-car-home \
+    googlecarui-com-android-car-media \
+    googlecarui-com-android-car-radio \
+    googlecarui-com-android-car-calendar \
+    googlecarui-com-android-car-companiondevicesupport \
+    googlecarui-com-android-car-systemupdater \
+    googlecarui-com-android-car-dialer \
+    googlecarui-com-android-car-linkviewer \
+    googlecarui-com-android-car-settings \
+    googlecarui-com-android-car-voicecontrol \
+    googlecarui-com-android-car-faceenroll \
+    googlecarui-com-android-permissioncontroller \
+    googlecarui-com-android-settings-intelligence \
+    googlecarui-com-google-android-apps-automotive-inputmethod \
+    googlecarui-com-google-android-apps-automotive-inputmethod-dev \
+    googlecarui-com-google-android-embedded-projection \
+    googlecarui-com-google-android-gms \
+    googlecarui-com-google-android-packageinstaller \
+    googlecarui-com-google-android-carassistant \
+    googlecarui-com-google-android-tts \
+    googlecarui-com-android-vending \
