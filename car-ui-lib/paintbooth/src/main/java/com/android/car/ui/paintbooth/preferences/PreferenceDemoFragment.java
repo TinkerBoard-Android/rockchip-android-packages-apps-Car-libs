107f6c5ade6a52a94aeb1ab44824fd41d5de1a4d
Cole Faust <colefaust@google.com>
2020-09-21 14:48:30 -0700

    Fix differences from our scuba tests
    * Change the number of collumns in the grid view activity
      from 3 back to 4, like it used to be
    * Remove the car_ui_activity_background from the recyclerviews
      and instead put it on views wrapping them, so the background
      doesn't show up in a scuba screenshot of just the recyclerview
    * Don't disable the switch preference because the scuba tests
      were flipping it on and checking it's on state.
    * Update copyPreference() to include the attributes of
      TwoStatePreference and SwitchPreference, because we were
      testing changing the subtitle of a SwitchPreference but
      the off/on subtitles weren't copied to the
      CarUiSwitchPreference
    
    Bug: 169105299
    Test: Manually
    Change-Id: Ic8a5c36de19263ea110f038c5909fdab63c257a3

--- car-ui-lib/paintbooth/src/main/java/com/android/car/ui/paintbooth/preferences/PreferenceDemoFragment.java
+++ car-ui-lib/paintbooth/src/main/java/com/android/car/ui/paintbooth/preferences/PreferenceDemoFragment.java
@@ -20,7 +20,6 @@ import android.os.Bundle;
 
 import com.android.car.ui.paintbooth.R;
 import com.android.car.ui.preference.CarUiPreference;
-import com.android.car.ui.preference.CarUiSwitchPreference;
 import com.android.car.ui.preference.PreferenceFragment;
 
 /**
@@ -45,10 +44,5 @@ public class PreferenceDemoFragment extends PreferenceFragment {
         preferenceDisabledWithRipple.setMessageToShowWhenDisabledPreferenceClicked(
                 "I am disabled because...");
         preferenceDisabledWithRipple.setShouldShowRippleOnDisabledPreference(true);
-
-        CarUiSwitchPreference carUiSwitchPreference = findPreference("switch");
-        carUiSwitchPreference.setEnabled(false);
-        carUiSwitchPreference.setMessageToShowWhenDisabledPreferenceClicked(
-                "I am disabled because...");
     }
 }
