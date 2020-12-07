c39b2738727a652c9fe43fae706af3c95bb85b0e
Priyank Singh <priyanksingh@google.com>
2020-12-07 12:02:02 -0800

    DO NOT MERGE Use content providers for sending data between apps
    Bug: 175030767
    Bug: 170343703
    Test: Manual
    Change-Id: Ibf2e61f8b2fc1a7d9a4d73011c8f793a0a3911b8

--- car-ui-lib/paintbooth/src/main/java/com/android/car/ui/paintbooth/widescreenime/WideScreenImeActivity.java
+++ car-ui-lib/paintbooth/src/main/java/com/android/car/ui/paintbooth/widescreenime/WideScreenImeActivity.java
@@ -22,25 +22,20 @@ import static com.android.car.ui.imewidescreen.CarUiImeWideScreenController.ADD_
 import static com.android.car.ui.imewidescreen.CarUiImeWideScreenController.ADD_DESC_TO_CONTENT_AREA;
 import static com.android.car.ui.imewidescreen.CarUiImeWideScreenController.ADD_ERROR_DESC_TO_INPUT_AREA;
 import static com.android.car.ui.imewidescreen.CarUiImeWideScreenController.REQUEST_RENDER_CONTENT_AREA;
-import static com.android.car.ui.imewidescreen.CarUiImeWideScreenController.SEARCH_RESULT_ICON_RES_ID_LIST;
-import static com.android.car.ui.imewidescreen.CarUiImeWideScreenController.SEARCH_RESULT_ITEM_ID_LIST;
-import static com.android.car.ui.imewidescreen.CarUiImeWideScreenController.SEARCH_RESULT_SUB_TITLE_LIST;
-import static com.android.car.ui.imewidescreen.CarUiImeWideScreenController.SEARCH_RESULT_SUPPLEMENTAL_ICON_ID_LIST;
-import static com.android.car.ui.imewidescreen.CarUiImeWideScreenController.SEARCH_RESULT_SUPPLEMENTAL_ICON_RES_ID_LIST;
-import static com.android.car.ui.imewidescreen.CarUiImeWideScreenController.SEARCH_RESULT_TITLE_LIST;
 import static com.android.car.ui.imewidescreen.CarUiImeWideScreenController.WIDE_SCREEN_ACTION;
 import static com.android.car.ui.imewidescreen.CarUiImeWideScreenController.WIDE_SCREEN_EXTRACTED_TEXT_ICON_RES_ID;
 
 import android.content.Context;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.text.Editable;
-import android.text.TextWatcher;
-import android.util.Pair;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.View.OnFocusChangeListener;
 import android.view.ViewGroup;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
+import android.widget.Switch;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -56,10 +51,12 @@ import com.android.car.ui.recyclerview.CarUiContentListItem;
 import com.android.car.ui.recyclerview.CarUiRecyclerView;
 import com.android.car.ui.toolbar.MenuItem;
 import com.android.car.ui.toolbar.Toolbar;
+import com.android.car.ui.toolbar.Toolbar.State;
 import com.android.car.ui.toolbar.ToolbarController;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 /**
  * Activity that shows different scenarios for wide screen ime.
@@ -69,8 +66,7 @@ public class WideScreenImeActivity extends AppCompatActivity implements InsetsCh
     private static final String TAG = "WideScreenImeActivity";
 
     private final List<MenuItem> mMenuItems = new ArrayList<>();
-    private final List<Pair<CharSequence, View.OnFocusChangeListener>> mEditText =
-            new ArrayList<>();
+    private final List<ListElement> mWidescreenItems = new ArrayList<>();
 
     private final ArrayList<String> mItemIdList = new ArrayList<>();
     private final ArrayList<String> mTitleList = new ArrayList<>();
@@ -78,6 +74,7 @@ public class WideScreenImeActivity extends AppCompatActivity implements InsetsCh
     private final ArrayList<Integer> mPrimaryImageResId = new ArrayList<>();
     private final ArrayList<String> mSecondaryItemId = new ArrayList<>();
     private final ArrayList<Integer> mSecondaryImageResId = new ArrayList<>();
+    private final List<CarUiImeSearchListItem> mSearchItems = new ArrayList<>();
 
     private InputMethodManager mInputMethodManager;
 
@@ -89,11 +86,7 @@ public class WideScreenImeActivity extends AppCompatActivity implements InsetsCh
         mInputMethodManager = (InputMethodManager)
                 getSystemService(Context.INPUT_METHOD_SERVICE);
 
-        ToolbarController toolbarNonFinal = CarUi.getToolbar(this);
-        if (toolbarNonFinal == null) {
-            toolbarNonFinal = requireViewById(R.id.toolbar);
-        }
-        ToolbarController toolbar = toolbarNonFinal;
+        ToolbarController toolbar = CarUi.getToolbar(this);
         toolbar.setTitle(getTitle());
         toolbar.setState(Toolbar.State.SUBPAGE);
         toolbar.setLogo(R.drawable.ic_launcher);
@@ -109,81 +102,63 @@ public class WideScreenImeActivity extends AppCompatActivity implements InsetsCh
 
         CarUiContentListItem.OnClickListener mainClickListener = i ->
                 Toast.makeText(this, "Item clicked!", Toast.LENGTH_SHORT).show();
-
         CarUiContentListItem.OnClickListener secondaryClickListener = i ->
                 Toast.makeText(this, "Item's secondary action clicked!", Toast.LENGTH_SHORT).show();
 
-        final int[] count = {1};
-        CarUiImeSearchListItem item = new CarUiImeSearchListItem(CarUiContentListItem.Action.ICON);
-        item.setTitle("Title " + count[0]);
-        item.setBody("Sub title " + count[0]);
-        item.setIconResId(R.drawable.ic_launcher);
-        item.setSupplementalIconResId(R.drawable.ic_launcher);
-        item.setSupplementalIcon(getDrawable(R.drawable.ic_launcher), secondaryClickListener);
-        item.setOnItemClickedListener(mainClickListener);
+        Drawable icon = getDrawable(R.drawable.ic_launcher);
 
-        List<CarUiImeSearchListItem> searchItems = new ArrayList<>();
-
-        searchItems.add(item);
-
-        // initial list to display in search view.
-        if (toolbar.canShowSearchResultItems()) {
-            toolbar.setSearchResultItems(searchItems);
-        }
-
-        LayoutInflater inflater = LayoutInflater.from(this);
-        View contentArea = inflater.inflate(R.layout.ime_wide_screen_dummy_view, null, true);
-
-        if (toolbar.canShowSearchResultsView()) {
-            toolbar.setSearchResultsView(contentArea);
-        }
-
-        contentArea.findViewById(R.id.button_1).setOnClickListener(v ->
-                Toast.makeText(this, "Button 1 clicked", Toast.LENGTH_SHORT).show()
-        );
-
-        contentArea.findViewById(R.id.button_2).setOnClickListener(v -> {
-                    Toast.makeText(this, "Clearing the view...", Toast.LENGTH_SHORT).show();
-                    toolbar.setSearchResultsView(null);
-                }
-        );
-
-        toolbar.registerOnSearchListener((query) -> {
-            count[0]++;
-            CarUiImeSearchListItem item1 = new CarUiImeSearchListItem(
+        for (int i = 1; i <= 100; i++) {
+            CarUiImeSearchListItem item = new CarUiImeSearchListItem(
                     CarUiContentListItem.Action.ICON);
-            item1.setTitle("Title " + count[0]);
-            item1.setBody("Sub title " + count[0]);
-            item1.setIconResId(R.drawable.ic_launcher);
-            item1.setSupplementalIconResId(R.drawable.ic_launcher);
-            item1.setSupplementalIcon(getDrawable(R.drawable.ic_launcher), secondaryClickListener);
-            item1.setOnItemClickedListener(mainClickListener);
-            searchItems.add(item1);
+            item.setTitle("Title " + i);
+            item.setBody("Sub title " + i);
+            item.setIcon(icon);
+            item.setSupplementalIcon(icon, secondaryClickListener);
+            item.setOnItemClickedListener(mainClickListener);
 
-            if (toolbar.canShowSearchResultItems()) {
-                toolbar.setSearchResultItems(searchItems);
-            }
-        });
+            mSearchItems.add(item);
+        }
+
+        AtomicBoolean showResultsInView = new AtomicBoolean(false);
 
         mMenuItems.add(MenuItem.builder(this)
                 .setToSearch()
                 .setOnClickListener(i -> {
-                    toolbar.setState(Toolbar.State.SEARCH);
+                    toolbar.setState(State.SEARCH);
+                    if (showResultsInView.get() && toolbar.canShowSearchResultsView()) {
+                        LayoutInflater inflater = LayoutInflater.from(this);
+                        View contentArea = inflater.inflate(R.layout.ime_wide_screen_dummy_view,
+                                null, true);
+                        contentArea.findViewById(R.id.button_1).setOnClickListener(v ->
+                                Toast.makeText(this, "Button 1 clicked", Toast.LENGTH_SHORT).show()
+                        );
+
+                        contentArea.findViewById(R.id.button_2).setOnClickListener(v -> {
+                                    Toast.makeText(this, "Clearing the view...",
+                                            Toast.LENGTH_SHORT).show();
+                                    toolbar.setSearchResultsView(null);
+                                }
+                        );
+                        toolbar.setSearchResultsView(contentArea);
+                    } else if (toolbar.canShowSearchResultItems()) {
+                        toolbar.setSearchResultsView(null);
+                        toolbar.setSearchResultItems(mSearchItems);
+                    }
                 })
                 .build());
 
         toolbar.setMenuItems(mMenuItems);
 
-        mEditText.add(Pair.create("Default Input Edit Text field", null));
+        mWidescreenItems.add(new ButtonElement("Show custom search view", v -> {
+            Switch swtch = (Switch) v;
+            showResultsInView.set(swtch.isChecked());
+        }));
 
-        mEditText.add(Pair.create("Add Desc to content area",
-                this::addDescToContentArea));
-
-        mEditText.add(Pair.create("Hide the content area",
-                this::hideContentArea));
-
-        mEditText.add(Pair.create("Hide extraction view",
-                this::hideExtractionView));
+        mWidescreenItems.add(new EditTextElement("Default Input Edit Text field", null));
+        mWidescreenItems.add(
+                new EditTextElement("Add Desc to content area", this::addDescToContentArea));
+        mWidescreenItems.add(new EditTextElement("Hide the content area", this::hideContentArea));
+        mWidescreenItems.add(new EditTextElement("Hide extraction view", this::hideExtractionView));
 
         for (int i = 0; i < 7; i++) {
             mItemIdList.add("itemId" + i);
@@ -194,12 +169,10 @@ public class WideScreenImeActivity extends AppCompatActivity implements InsetsCh
             mSecondaryImageResId.add(R.drawable.ic_launcher);
         }
 
-        mEditText.add(Pair.create("Show IME list view", this::showImeListView));
-
-        mEditText.add(Pair.create("Add icon to extracted view", this::addIconToExtractedView));
-
-        mEditText.add(
-                Pair.create("Add error message to content area", this::addErrorDescToContentArea));
+        mWidescreenItems.add(
+                new EditTextElement("Add icon to extracted view", this::addIconToExtractedView));
+        mWidescreenItems.add(new EditTextElement("Add error message to content area",
+                this::addErrorDescToContentArea));
 
         CarUiRecyclerView recyclerView = requireViewById(R.id.list);
         recyclerView.setAdapter(mAdapter);
@@ -224,52 +197,6 @@ public class WideScreenImeActivity extends AppCompatActivity implements InsetsCh
         bundle.putString(ADD_ERROR_DESC_TO_INPUT_AREA, "Some error message");
         bundle.putString(ADD_DESC_TITLE_TO_CONTENT_AREA, "Title");
         bundle.putString(ADD_DESC_TO_CONTENT_AREA, "Description provided by the application");
-        mInputMethodManager.sendAppPrivateCommand(view, WIDE_SCREEN_ACTION, bundle);
-    }
-
-    private void showImeListView(View view, boolean hasFocus) {
-        if (!hasFocus) {
-            return;
-        }
-
-        Bundle bundle = new Bundle();
-
-        EditText editText = (EditText) view;
-        editText.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-                mItemIdList.add("itemId " + s.toString());
-                mTitleList.add("Title " + s.toString());
-                mSubTitleList.add("subtitle ");
-                mPrimaryImageResId.add(R.drawable.ic_launcher);
-                mSecondaryItemId.add("imageId" + s.toString());
-                mSecondaryImageResId.add(R.drawable.ic_launcher);
-
-                bundle.putStringArrayList(SEARCH_RESULT_TITLE_LIST, mTitleList);
-                bundle.putStringArrayList(SEARCH_RESULT_SUB_TITLE_LIST, mSubTitleList);
-                bundle.putIntegerArrayList(SEARCH_RESULT_ICON_RES_ID_LIST,
-                        mPrimaryImageResId);
-                mInputMethodManager.sendAppPrivateCommand(view, WIDE_SCREEN_ACTION, bundle);
-            }
-        });
-
-        bundle.putStringArrayList(SEARCH_RESULT_ITEM_ID_LIST, mItemIdList);
-        bundle.putStringArrayList(SEARCH_RESULT_TITLE_LIST, mTitleList);
-        bundle.putStringArrayList(SEARCH_RESULT_SUB_TITLE_LIST, mSubTitleList);
-        bundle.putStringArrayList(SEARCH_RESULT_SUPPLEMENTAL_ICON_ID_LIST, mSecondaryItemId);
-        bundle.putIntegerArrayList(SEARCH_RESULT_ICON_RES_ID_LIST, mPrimaryImageResId);
-        bundle.putIntegerArrayList(SEARCH_RESULT_SUPPLEMENTAL_ICON_RES_ID_LIST,
-                mSecondaryImageResId);
         mInputMethodManager.sendAppPrivateCommand(view, WIDE_SCREEN_ACTION, bundle);
     }
 
@@ -308,18 +235,51 @@ public class WideScreenImeActivity extends AppCompatActivity implements InsetsCh
     }
 
 
-    private static class ViewHolder extends RecyclerView.ViewHolder {
+    private abstract static class ViewHolder extends RecyclerView.ViewHolder {
 
+        ViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+
+        public abstract void bind(ListElement element);
+    }
+
+    private static class EditTextViewHolder extends ViewHolder {
         private final EditText mEditText;
 
-        ViewHolder(View itemView) {
+        EditTextViewHolder(@NonNull View itemView) {
             super(itemView);
             mEditText = itemView.requireViewById(R.id.edit_text);
         }
 
-        public void bind(CharSequence title, View.OnFocusChangeListener listener) {
-            mEditText.setText(title);
-            mEditText.setOnFocusChangeListener(listener);
+        @Override
+        public void bind(ListElement e) {
+            if (!(e instanceof EditTextElement)) {
+                throw new IllegalArgumentException("Expected an EditTextElement");
+            }
+            EditTextElement element = (EditTextElement) e;
+            mEditText.setText(element.getText());
+            mEditText.setOnFocusChangeListener(element.getOnFocusChangeListener());
+        }
+    }
+
+    private static class ButtonViewHolder extends ViewHolder {
+        private final Switch mSwitch;
+
+        ButtonViewHolder(@NonNull View itemView) {
+            super(itemView);
+            mSwitch = itemView.requireViewById(R.id.button);
+        }
+
+        @Override
+        public void bind(ListElement e) {
+            if (!(e instanceof ButtonElement)) {
+                throw new IllegalArgumentException("Expected an ButtonElement");
+            }
+            ButtonElement element = (ButtonElement) e;
+            mSwitch.setText(element.getText());
+            mSwitch.setOnClickListener(element.getOnClickListener());
+            mSwitch.setChecked(false);
         }
     }
 
@@ -327,23 +287,31 @@ public class WideScreenImeActivity extends AppCompatActivity implements InsetsCh
             new RecyclerView.Adapter<ViewHolder>() {
                 @Override
                 public int getItemCount() {
-                    return mEditText.size();
+                    return mWidescreenItems.size();
                 }
 
                 @Override
-                public ViewHolder onCreateViewHolder(ViewGroup parent, int position) {
-                    View item =
-                            LayoutInflater.from(parent.getContext())
-                                    .inflate(R.layout.edit_text_list_item,
-                                            parent, false);
-
-                    return new ViewHolder(item);
+                public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+                    LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+                    if (viewType == ListElement.TYPE_EDIT_TEXT) {
+                        return new EditTextViewHolder(
+                                inflater.inflate(R.layout.edit_text_list_item, parent, false));
+                    } else if (viewType == ListElement.TYPE_BUTTON) {
+                        return new ButtonViewHolder(
+                                inflater.inflate(R.layout.list_item_switch, parent, false));
+                    } else {
+                        throw new IllegalArgumentException("Unknown viewType: " + viewType);
+                    }
                 }
 
                 @Override
                 public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-                    Pair<CharSequence, View.OnFocusChangeListener> pair = mEditText.get(position);
-                    holder.bind(pair.first, pair.second);
+                    holder.bind(mWidescreenItems.get(position));
+                }
+
+                @Override
+                public int getItemViewType(int position) {
+                    return mWidescreenItems.get(position).getType();
                 }
             };
 
@@ -353,5 +321,58 @@ public class WideScreenImeActivity extends AppCompatActivity implements InsetsCh
                 .setPadding(0, insets.getTop(), 0, insets.getBottom());
         requireViewById(android.R.id.content)
                 .setPadding(insets.getLeft(), 0, insets.getRight(), 0);
+    }
+
+    private abstract static class ListElement {
+        static final int TYPE_EDIT_TEXT = 0;
+        static final int TYPE_BUTTON = 1;
+
+        private final String mText;
+
+        ListElement(String text) {
+            mText = text;
+        }
+
+        String getText() {
+            return mText;
+        }
+
+        abstract int getType();
+    }
+
+    private static class EditTextElement extends ListElement {
+        private OnFocusChangeListener mListener;
+
+        EditTextElement(String text, OnFocusChangeListener listener) {
+            super(text);
+            mListener = listener;
+        }
+
+        OnFocusChangeListener getOnFocusChangeListener() {
+            return mListener;
+        }
+
+        @Override
+        int getType() {
+            return TYPE_EDIT_TEXT;
+        }
+    }
+
+    private static class ButtonElement extends ListElement {
+        private OnClickListener mOnClickListener;
+
+        ButtonElement(String text, OnClickListener listener) {
+            super(text);
+            mOnClickListener = listener;
+        }
+
+        public OnClickListener getOnClickListener() {
+            return mOnClickListener;
+        }
+
+        @Override
+        int getType() {
+            return TYPE_BUTTON;
+        }
     }
 }
