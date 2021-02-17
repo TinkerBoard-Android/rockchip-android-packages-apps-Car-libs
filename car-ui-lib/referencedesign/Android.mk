4afc9ff8881c2c4e20bef8755a2a8a346cdddf5a
Cole Faust <colefaust@google.com>
2021-02-17 12:10:40 -0800

    Make all car-ui-lib instances use an overlayable file
    As apps add their own overlayable files, car-ui-lib rros
    become required to target overlayable groups, and the apps
    must also use a version of car-ui-lib with an overlayable file.
    Get ahead of the curve by adopting overlayable files in
    all apps for S.
    
    Having an overlayable file also allows restricting what
    can be overlaid. As car-ui-lib moves to shared libraries for
    customization, it will want to prevent overlaying certain
    components that are shared lib only.
    
    Bug: 180521536
    Test: Manually
    Change-Id: I70c7c4d90ab80f777b3d3553f2b931522efb5725

--- car-ui-lib/referencedesign/Android.mk
+++ car-ui-lib/referencedesign/Android.mk
@@ -5,6 +5,23 @@ CAR_UI_RRO_SET_NAME := googlecarui
 CAR_UI_RRO_MANIFEST_FILE := $(LOCAL_PATH)/AndroidManifest.xml
 CAR_UI_RESOURCE_DIR := $(LOCAL_PATH)/res
 CAR_UI_RRO_TARGETS := \
+    com.google.android.apps.automotive.inputmethod \
+    com.google.android.apps.automotive.inputmethod.dev \
+    com.google.android.apps.automotive.templates.host \
+    com.google.android.embedded.projection \
+    com.google.android.gms \
+    com.google.android.gsf \
+    com.google.android.packageinstaller \
+    com.google.android.carassistant \
+    com.google.android.tts \
+    com.android.vending \
+
+include packages/apps/Car/libs/car-ui-lib/generate_rros.mk
+
+CAR_UI_RRO_SET_NAME := googlecarui.overlayable
+CAR_UI_RRO_MANIFEST_FILE := $(LOCAL_PATH)/AndroidManifest-overlayable.xml
+CAR_UI_RESOURCE_DIR := $(LOCAL_PATH)/res
+CAR_UI_RRO_TARGETS := \
     com.android.car.ui.paintbooth \
     com.google.android.car.ui.paintbooth \
     com.android.car.rotaryplayground \
@@ -23,23 +40,6 @@ CAR_UI_RRO_TARGETS := \
     com.android.car.faceenroll \
     com.android.managedprovisioning \
     com.android.settings.intelligence \
-    com.google.android.apps.automotive.inputmethod \
-    com.google.android.apps.automotive.inputmethod.dev \
-    com.google.android.apps.automotive.templates.host \
-    com.google.android.embedded.projection \
-    com.google.android.gms \
-    com.google.android.gsf \
-    com.google.android.packageinstaller \
-    com.google.android.carassistant \
-    com.google.android.tts \
-    com.android.vending \
-
-include packages/apps/Car/libs/car-ui-lib/generate_rros.mk
-
-CAR_UI_RRO_SET_NAME := googlecarui.overlayable
-CAR_UI_RRO_MANIFEST_FILE := $(LOCAL_PATH)/AndroidManifest-overlayable.xml
-CAR_UI_RESOURCE_DIR := $(LOCAL_PATH)/res
-CAR_UI_RRO_TARGETS := \
     com.google.android.apps.automotive.inputmethod \
     com.google.android.apps.automotive.inputmethod.dev \
     com.google.android.apps.automotive.templates.host \
