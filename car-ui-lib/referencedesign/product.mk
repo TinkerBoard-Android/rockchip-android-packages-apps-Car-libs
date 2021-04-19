e3bb8bd90c19c5eb21a0503146a267f1da28fd56
Cole Faust <colefaust@google.com>
2021-04-19 13:10:50 -0700

    Remove car-ui-lib-overlayable
    car-ui-lib now include an overlayable.xml file
    by default, so there's no reasong to have a
    separate build target.
    
    Fixes: 185809785
    Test: Manually
    Change-Id: Iad9ddb6b26b576067b92e3c67cb4ae263ec04d1d

--- car-ui-lib/referencedesign/product.mk
+++ car-ui-lib/referencedesign/product.mk
@@ -12,6 +12,25 @@ PRODUCT_COPY_FILES += \
 
 # Include generated RROs
 PRODUCT_PACKAGES += \
+    googlecarui-com-android-car-ui-paintbooth \
+    googlecarui-com-google-android-car-ui-paintbooth \
+    googlecarui-com-google-android-carui-ats \
+    googlecarui-com-android-car-rotaryplayground \
+    googlecarui-com-android-car-themeplayground \
+    googlecarui-com-android-car-carlauncher \
+    googlecarui-com-android-car-home \
+    googlecarui-com-android-car-media \
+    googlecarui-com-android-car-radio \
+    googlecarui-com-android-car-calendar \
+    googlecarui-com-android-car-companiondevicesupport \
+    googlecarui-com-android-car-systemupdater \
+    googlecarui-com-android-car-dialer \
+    googlecarui-com-android-car-linkviewer \
+    googlecarui-com-android-car-settings \
+    googlecarui-com-android-car-voicecontrol \
+    googlecarui-com-android-car-faceenroll \
+    googlecarui-com-android-managedprovisioning \
+    googlecarui-com-android-settings-intelligence \
     googlecarui-com-google-android-apps-automotive-inputmethod \
     googlecarui-com-google-android-apps-automotive-inputmethod-dev \
     googlecarui-com-google-android-apps-automotive-templates-host \
@@ -19,43 +38,10 @@ PRODUCT_PACKAGES += \
     googlecarui-com-google-android-gms \
     googlecarui-com-google-android-gsf \
     googlecarui-com-google-android-packageinstaller \
+    googlecarui-com-google-android-permissioncontroller \
     googlecarui-com-google-android-carassistant \
     googlecarui-com-google-android-tts \
     googlecarui-com-android-vending \
-
-
-# Include generated RROs that that use targetName
-PRODUCT_PACKAGES += \
-    googlecarui-overlayable-com-android-car-ui-paintbooth \
-    googlecarui-overlayable-com-google-android-car-ui-paintbooth \
-    googlecarui-overlayable-com-google-android-carui-ats \
-    googlecarui-overlayable-com-android-car-rotaryplayground \
-    googlecarui-overlayable-com-android-car-themeplayground \
-    googlecarui-overlayable-com-android-car-carlauncher \
-    googlecarui-overlayable-com-android-car-home \
-    googlecarui-overlayable-com-android-car-media \
-    googlecarui-overlayable-com-android-car-radio \
-    googlecarui-overlayable-com-android-car-calendar \
-    googlecarui-overlayable-com-android-car-companiondevicesupport \
-    googlecarui-overlayable-com-android-car-systemupdater \
-    googlecarui-overlayable-com-android-car-dialer \
-    googlecarui-overlayable-com-android-car-linkviewer \
-    googlecarui-overlayable-com-android-car-settings \
-    googlecarui-overlayable-com-android-car-voicecontrol \
-    googlecarui-overlayable-com-android-car-faceenroll \
-    googlecarui-overlayable-com-android-managedprovisioning \
-    googlecarui-overlayable-com-android-settings-intelligence \
-    googlecarui-overlayable-com-google-android-apps-automotive-inputmethod \
-    googlecarui-overlayable-com-google-android-apps-automotive-inputmethod-dev \
-    googlecarui-overlayable-com-google-android-apps-automotive-templates-host \
-    googlecarui-overlayable-com-google-android-embedded-projection \
-    googlecarui-overlayable-com-google-android-gms \
-    googlecarui-overlayable-com-google-android-gsf \
-    googlecarui-overlayable-com-google-android-packageinstaller \
-    googlecarui-overlayable-com-google-android-permissioncontroller \
-    googlecarui-overlayable-com-google-android-carassistant \
-    googlecarui-overlayable-com-google-android-tts \
-    googlecarui-overlayable-com-android-vending \
 
 # This system property is used to enable the RROs on startup via
 # the requiredSystemPropertyName/Value attributes in the manifest
