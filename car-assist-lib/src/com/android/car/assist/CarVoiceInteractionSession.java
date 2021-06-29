4dd023ff58a15dda259ee8c838b681638615c223
Uchenna Okoye <uokoye@google.com>
2021-06-29 21:44:34 +0000

    Send a Notification Access Exception when Assistant does not have notification access
    Bug: 192259026
    Change-Id: I18c27f5a7c0fbf2383d30e37d676372130762c6e
    PiperOrigin-RevId: 382115766

--- car-assist-lib/src/com/android/car/assist/CarVoiceInteractionSession.java
+++ car-assist-lib/src/com/android/car/assist/CarVoiceInteractionSession.java
@@ -23,6 +23,7 @@ import android.service.voice.VoiceInteractionService;
 import android.service.voice.VoiceInteractionSession;
 
 import androidx.annotation.StringDef;
+import androidx.core.app.NotificationManagerCompat;
 
 import com.android.car.assist.payloadhandlers.NotificationPayloadHandler;
 import com.android.car.messenger.common.Conversation;
@@ -43,18 +44,25 @@ public abstract class CarVoiceInteractionSession extends VoiceInteractionSession
     public static final String KEY_EXCEPTION = "KEY_EXCEPTION";
 
     /**
-     * The key used for the {@link CarVoiceInteractionSession#VOICE_ACTION_HANDLE_EXCEPTION}
-     * payload
+     * The key used for the {@link CarVoiceInteractionSession#VOICE_ACTION_HANDLE_EXCEPTION} payload
      * {@link Bundle}. Must map to a boolean. If value is true, the Fallback Assistant that can
-     * handle
-     * the user's request has been disabled.
+     * handle the user's request has been disabled.
      */
     public static final String KEY_FALLBACK_ASSISTANT_ENABLED = "KEY_FALLBACK_ASSISTANT_ENABLED";
 
     /**
+     * The key used for a substitute package name the Digital Assistant should read out in lieu of
+     * package name associated with the {@link StatusBarNotification}.
+     *
+     * <p>Only system packages which lump together a bunch of unrelated stuff may substitute a
+     * different name to make the purpose of the notification more clear. The correct package label
+     * should always be accessible via SystemUI.
+     */
+    public static final String EXTRA_SUBSTITUTE_APP_NAME = "android.substName";
+
+    /**
      * The key used for the payload {@link Bundle}, if a {@link StatusBarNotification} is used as
-     * the
-     * payload.
+     * the payload.
      */
     public static final String KEY_NOTIFICATION = "KEY_NOTIFICATION";
 
@@ -68,39 +76,36 @@ public abstract class CarVoiceInteractionSession extends VoiceInteractionSession
     public static final String VOICE_ACTION_NO_ACTION = "VOICE_ACTION_NO_ACTION";
 
     /**
-     * Indicates to assistant that a read action is being requested for a given payload.
-     * A {@link StatusBarNotification} object will be provided in the payload
+     * Indicates to assistant that a read action is being requested for a given payload. A {@link
+     * StatusBarNotification} object will be provided in the payload
      */
     public static final String VOICE_ACTION_READ_NOTIFICATION = "VOICE_ACTION_READ_NOTIFICATION";
 
     /**
-     * Indicates to assistant that a reply action is being requested for a given payload.
-     * A {@link StatusBarNotification} object will be provided in the payload
+     * Indicates to assistant that a reply action is being requested for a given payload. A {@link
+     * StatusBarNotification} object will be provided in the payload
      */
     public static final String VOICE_ACTION_REPLY_NOTIFICATION = "VOICE_ACTION_REPLY_NOTIFICATION";
 
     /**
-     * Indicates to assistant that a read conversation action is being requested.
-     * A {@link Conversation} object will be provided in the payload.
+     * Indicates to assistant that a read conversation action is being requested. A {@link
+     * Conversation} object will be provided in the payload.
      */
     public static final String VOICE_ACTION_READ_CONVERSATION = "VOICE_ACTION_READ_CONVERSATION";
 
     /**
-     * Indicates to assistant that a reply conversation action is being requested.
-     * A {@link Conversation} object will be provided in the payload.
+     * Indicates to assistant that a reply conversation action is being requested. A {@link
+     * Conversation} object will be provided in the payload.
      */
     public static final String VOICE_ACTION_REPLY_CONVERSATION = "VOICE_ACTION_REPLY_CONVERSATION";
 
     /**
      * Indicates to digital assistant that it should capture a SMS message from the user,
-     * potentially
-     * finding which contact to send the message to and which device to send the message from
-     * (only if the application does not send the digital assistant this information in the
-     * bundle).
-     * Once the digital assistant has gathered the information from the user, it should send back
-     * the PendingIntent (provided in the bundle) with the information so the application can
-     * actually
-     * send the SMS.
+     * potentially finding which contact to send the message to and which device to send the message
+     * from (only if the application does not send the digital assistant this information in the
+     * bundle). Once the digital assistant has gathered the information from the user, it should
+     * send back the PendingIntent (provided in the bundle) with the information so the application
+     * can actually send the SMS.
      */
     public static final String VOICE_ACTION_SEND_SMS = "VOICE_ACTION_SEND_SMS";
 
@@ -112,8 +117,8 @@ public abstract class CarVoiceInteractionSession extends VoiceInteractionSession
 
     /**
      * Recipient's name. If this and the recipient phone number are not provided by the application,
-     * digital assistant must do contact disambiguation but is not required to add the name
-     * to the PendingIntent.
+     * digital assistant must do contact disambiguation but is not required to add the name to the
+     * PendingIntent.
      */
     public static final String KEY_RECIPIENT_NAME = "KEY RECIPIENT NAME";
 
@@ -151,8 +156,7 @@ public abstract class CarVoiceInteractionSession extends VoiceInteractionSession
 
     /** The list of exceptions the active voice service must handle. */
     @StringDef({EXCEPTION_NOTIFICATION_LISTENER_PERMISSIONS_MISSING})
-    public @interface ExceptionValue {
-    }
+    public @interface ExceptionValue {}
 
     /**
      * Indicates to assistant that it is missing the Notification Listener permission, and should
@@ -184,7 +188,8 @@ public abstract class CarVoiceInteractionSession extends VoiceInteractionSession
     @Override
     public final void onShow(Bundle args, int showFlags) {
         super.onShow(args, showFlags);
-        if (args != null && isCarNotificationSource(showFlags)) {
+        addNotificationAccessExceptionIfNeeded(args);
+        if (args != null) {
             String action = getRequestedVoiceAction(args);
             if (!VOICE_ACTION_NO_ACTION.equals(action)) {
                 onShow(action, args, showFlags);
@@ -197,25 +202,53 @@ public abstract class CarVoiceInteractionSession extends VoiceInteractionSession
     /**
      * Called when the session UI is going to be shown. This is called after {@link
      * #onCreateContentView} (if the session's content UI needed to be created) and immediately
-     * prior
-     * to the window being shown. This may be called while the window is already shown, if a show
-     * request has come in while it is shown, to allow you to update the UI to match the new show
-     * arguments.
+     * prior to the window being shown. This may be called while the window is already shown, if a
+     * show request has come in while it is shown, to allow you to update the UI to match the new
+     * show arguments.
      *
      * @param action The action that is being requested for this session (e.g. {@link
-     *               CarVoiceInteractionSession#VOICE_ACTION_READ_NOTIFICATION}, {@link
-     *               CarVoiceInteractionSession#VOICE_ACTION_REPLY_NOTIFICATION}).
-     * @param args   The arguments that were supplied to {@link VoiceInteractionService#showSession
-     *               VoiceInteractionService.showSession}.
-     * @param flags  The show flags originally provided to
-     * {@link VoiceInteractionService#showSession
-     *               VoiceInteractionService.showSession}.
+     *     CarVoiceInteractionSession#VOICE_ACTION_READ_NOTIFICATION}, {@link
+     *     CarVoiceInteractionSession#VOICE_ACTION_REPLY_NOTIFICATION}).
+     * @param args The arguments that were supplied to {@link VoiceInteractionService#showSession
+     *     VoiceInteractionService.showSession}.
+     * @param flags The show flags originally provided to {@link VoiceInteractionService#showSession
+     *     VoiceInteractionService.showSession}.
      */
     protected abstract void onShow(String action, Bundle args, int flags);
 
-    /** Returns true if the request was initiated for a car notification. */
-    private static boolean isCarNotificationSource(int flags) {
-        return (flags & SHOW_SOURCE_NOTIFICATION) != 0;
+    /**
+     * Transforms bundle to {@link KEY_EXCEPTION} with
+     * value {@link EXCEPTION_NOTIFICATION_LISTENER_PERMISSIONS_MISSING} if
+     * Notification Listener permissions are missing.
+     */
+    private void addNotificationAccessExceptionIfNeeded(Bundle args) {
+      if (needsNotificationAccess(args)) {
+              args.putString(KEY_ACTION, VOICE_ACTION_HANDLE_EXCEPTION);
+              args.putString(KEY_EXCEPTION, EXCEPTION_NOTIFICATION_LISTENER_PERMISSIONS_MISSING);
+              args.putBoolean(KEY_FALLBACK_ASSISTANT_ENABLED, false);
+      }
+    }
+
+    private boolean needsNotificationAccess(Bundle args) {
+      return isNotificationAction(args) && !hasNotificationAccess(getContext());
+    }
+
+    /**
+     * Returns {@code true} if the given {@code args} is a notification action, {@code false}
+     * otherwise
+     */
+    private static boolean isNotificationAction(Bundle args) {
+        if (args == null) {
+          return false;
+        }
+        String action = args.getString(KEY_ACTION);
+        return VOICE_ACTION_REPLY_NOTIFICATION.equals(action)
+            || VOICE_ACTION_READ_NOTIFICATION.equals(action);
+    }
+
+    private boolean hasNotificationAccess(Context context) {
+        return NotificationManagerCompat
+            .getEnabledListenerPackages(context).contains(context.getPackageName());
     }
 
     /**
