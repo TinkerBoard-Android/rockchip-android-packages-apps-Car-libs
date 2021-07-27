3e6c17f5a4cd37fbd5c30e3d21f2282143a6aeae
Cole Faust <colefaust@google.com>
2021-07-26 17:23:54 -0700

    Change plugin's dialog to a base layout layer
    The sourceContext can not always be used to create a dialog.
    Instead, use the base layout to craft our own dialog.
    
    Fixes: 194233067
    Test: Manually
    Change-Id: Id6f342f18b73604f0decead4777a4e939cad218a

--- car-ui-lib/referencedesign/plugin/src/main/java/com/chassis/car/ui/plugin/toolbar/OverflowMenuItem.java
+++ car-ui-lib/referencedesign/plugin/src/main/java/com/chassis/car/ui/plugin/toolbar/OverflowMenuItem.java
@@ -15,12 +15,15 @@
  */
 package com.chassis.car.ui.plugin.toolbar;
 
-import android.app.AlertDialog;
-import android.app.Dialog;
+import android.animation.Animator;
 import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewStub;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.core.content.ContextCompat;
 
 import com.android.car.ui.plugin.oemapis.toolbar.MenuItemOEMV1;
@@ -31,22 +34,24 @@ import java.util.Collections;
 import java.util.List;
 
 class OverflowMenuItem {
+    private static final int DIALOG_ANIMATION_TIME = 250;
 
     @NonNull
-    private final Context mSourceContext;
+    private final Context mPluginContext;
 
     @NonNull
     private List<MenuItemOEMV1> mOverflowMenuItems = Collections.emptyList();
 
-    @Nullable
-    private Dialog mDialog;
-
     private MenuItemOEMV1 mMenuItem;
+
+    private LinearLayout mItemsContainer;
+    private View mDialogBackground;
+
 
     OverflowMenuItem(
             @NonNull Context pluginContext,
-            @NonNull Context sourceContext) {
-        mSourceContext = sourceContext;
+            @NonNull ViewStub dialogStub) {
+        mPluginContext = pluginContext;
 
         mMenuItem = MenuItemOEMV1.builder()
                 .setTitle(pluginContext.getString(R.string.toolbar_menu_item_overflow_title))
@@ -54,26 +59,93 @@ class OverflowMenuItem {
                         pluginContext, R.drawable.toolbar_menu_item_overflow))
                 .setVisible(false)
                 .setOnClickListener(() -> {
-                    String[] titles = mOverflowMenuItems.stream()
-                            .map(MenuItemOEMV1::getTitle)
-                            .toArray(String[]::new);
+                    if (mItemsContainer == null) {
+                        mDialogBackground = dialogStub.inflate();
+                        mItemsContainer = mDialogBackground
+                                .requireViewById(R.id.toolbar_dialog_linear_layout);
+                        mDialogBackground.requireViewById(R.id.toolbar_dialog_dismiss_button)
+                                .setOnClickListener(v -> hideDialog());
+                        mDialogBackground.requireViewById(R.id.toolbar_dialog_shade)
+                                .setOnClickListener(v -> hideDialog());
+                    }
 
-                    // TODO(b/194233067) Do not create dialogs using the source context, it is
-                    //                   not always an activity context and will crash in cases
-                    //                   where it isn't. Replace this with a layer in the base
-                    //                   layout that looks like a dialog.
-                    mDialog = new AlertDialog.Builder(sourceContext)
-                            .setItems(titles, (dialog, which) -> {
-                                Runnable onClickListener = mOverflowMenuItems.get(which)
-                                        .getOnClickListener();
-                                if (onClickListener != null) {
-                                    onClickListener.run();
-                                }
-                                dialog.dismiss();
-                            }).create();
-                    mDialog.show();
+                    refreshViews();
+
+                    mDialogBackground.setVisibility(View.VISIBLE);
+                    mDialogBackground.setAlpha(0);
+                    mDialogBackground.animate()
+                            .alpha(1)
+                            .setDuration(DIALOG_ANIMATION_TIME)
+                            .setListener(null)
+                            .start();
                 })
                 .build();
+    }
+
+    private void hideDialog() {
+        if (mDialogBackground.getVisibility() == View.GONE) {
+            return;
+        }
+        mDialogBackground.animate()
+                .alpha(0)
+                .setDuration(DIALOG_ANIMATION_TIME)
+                .setListener(
+                        new Animator.AnimatorListener() {
+                            @Override
+                            public void onAnimationStart(Animator animation) {
+                            }
+
+                            @Override
+                            public void onAnimationEnd(Animator animation) {
+                                mDialogBackground.setVisibility(View.GONE);
+                            }
+
+                            @Override
+                            public void onAnimationCancel(Animator animation) {
+                                mDialogBackground.setVisibility(View.GONE);
+                            }
+
+                            @Override
+                            public void onAnimationRepeat(Animator animation) {
+                            }
+                        })
+                .start();
+    }
+
+    private void refreshViews() {
+        if (mItemsContainer == null) {
+            return;
+        }
+
+        // Make mItemsContainer have a child toolbar_overflow_item for each overflow menu item.
+        if (mItemsContainer.getChildCount() > mOverflowMenuItems.size()) {
+            mItemsContainer.removeViews(mOverflowMenuItems.size(),
+                    mItemsContainer.getChildCount() - mOverflowMenuItems.size());
+        }
+        while (mItemsContainer.getChildCount() < mOverflowMenuItems.size()) {
+            LayoutInflater.from(mPluginContext).inflate(
+                    R.layout.toolbar_overflow_item, mItemsContainer, true);
+        }
+
+        for (int i = 0; i < mOverflowMenuItems.size(); i++) {
+            bindItemView(mItemsContainer.getChildAt(i), mOverflowMenuItems.get(i));
+        }
+    }
+
+    private void bindItemView(View view, MenuItemOEMV1 item) {
+        TextView titleView = view.requireViewById(R.id.toolbar_overflow_item_title);
+        titleView.setText(item.getTitle());
+
+        Runnable onClickListener = item.getOnClickListener();
+        if (onClickListener != null) {
+            view.setOnClickListener(v -> {
+                hideDialog();
+                onClickListener.run();
+            });
+        } else {
+            view.setOnClickListener(null);
+            view.setClickable(false);
+        }
     }
 
     public MenuItemOEMV1 getMenuItem() {
@@ -84,9 +156,8 @@ class OverflowMenuItem {
         mOverflowMenuItems = menuItems;
         mMenuItem = mMenuItem.copy().setVisible(!menuItems.isEmpty()).build();
 
-        if (mDialog != null) {
-            mDialog.dismiss();
-            mDialog = null;
+        if (mDialogBackground != null && mDialogBackground.getVisibility() == View.VISIBLE) {
+            refreshViews();
         }
     }
 }
