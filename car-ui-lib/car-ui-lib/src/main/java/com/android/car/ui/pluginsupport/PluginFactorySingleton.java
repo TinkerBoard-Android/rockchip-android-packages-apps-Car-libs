ca27905a76de9c4a154b46444e9803ed789ed691
Ram Parameswaran <rampara@google.com>
2021-10-07 04:33:52 +0000

    Remove use of system properties for plugin package name and enabled state
    Bug: 200608959
    Test: Manual
    
    Change-Id: I31531b3d86bd0047f597831d91b6c7d2ba1e5794

--- car-ui-lib/car-ui-lib/src/main/java/com/android/car/ui/pluginsupport/PluginFactorySingleton.java
+++ car-ui-lib/car-ui-lib/src/main/java/com/android/car/ui/pluginsupport/PluginFactorySingleton.java
@@ -15,15 +15,21 @@
  */
 package com.android.car.ui.pluginsupport;
 
+import static android.content.pm.PackageManager.COMPONENT_ENABLED_STATE_ENABLED;
+import static android.content.pm.PackageManager.MATCH_ALL;
+import static android.content.pm.PackageManager.MATCH_DISABLED_COMPONENTS;
+
 import static com.android.car.ui.core.CarUi.MIN_TARGET_API;
 
 import static java.util.Objects.requireNonNull;
 
 import android.annotation.SuppressLint;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
+import android.content.pm.ProviderInfo;
 import android.os.Build;
 import android.os.Process;
 import android.text.TextUtils;
@@ -35,7 +41,6 @@ import androidx.annotation.RequiresApi;
 import androidx.annotation.VisibleForTesting;
 
 import com.android.car.ui.R;
-import com.android.car.ui.utils.CarUiUtils;
 
 import java.io.File;
 import java.lang.reflect.Method;
@@ -99,8 +104,7 @@ public final class PluginFactorySingleton {
             return sInstance;
         }
 
-        String pluginPackageName = CarUiUtils.getSystemProperty(context.getResources(),
-                R.string.car_ui_plugin_package_system_property_name);
+        String pluginPackageName = getPluginPackageName(context);
 
         if (TextUtils.isEmpty(pluginPackageName)) {
             sInstance = new PluginFactoryStub();
@@ -250,8 +254,41 @@ public final class PluginFactorySingleton {
         return abis;
     }
 
-    private static boolean isPluginEnabled(Context context) {
-        return CarUiUtils.getBooleanSystemProperty(context.getResources(),
-                R.string.car_ui_plugin_enabled_system_property_name, false);
+    /**
+     * Return the package name for the Car UI plugin implementation.
+     */
+    @Nullable
+    public static String getPluginPackageName(Context context) {
+        String authority = context.getString(
+                R.string.car_ui_plugin_package_provider_authority_name);
+        ProviderInfo providerInfo = context.getPackageManager().resolveContentProvider(authority,
+                MATCH_ALL | MATCH_DISABLED_COMPONENTS);
+        if (providerInfo == null) {
+            return null;
+        }
+        return providerInfo.packageName;
+    }
+
+    /**
+     * Return if Car UI components should be loaded from the plugin implementation.
+     */
+    public static boolean isPluginEnabled(Context context) {
+        String authority = context.getString(
+                R.string.car_ui_plugin_package_provider_authority_name);
+        PackageManager packageManager = context.getPackageManager();
+        ProviderInfo providerInfo = packageManager.resolveContentProvider(authority,
+                MATCH_ALL | MATCH_DISABLED_COMPONENTS);
+        if (providerInfo == null) {
+            return false;
+        }
+
+        ComponentName componentName = new ComponentName(providerInfo.packageName,
+                providerInfo.name);
+        int state = packageManager.getComponentEnabledSetting(componentName);
+        if (state == PackageManager.COMPONENT_ENABLED_STATE_DEFAULT) {
+            return providerInfo.enabled;
+        }
+
+        return state == COMPONENT_ENABLED_STATE_ENABLED;
     }
 }
